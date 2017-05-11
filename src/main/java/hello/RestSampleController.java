efbf2cf7d92d862e616e287f120fc795df734f42
Shinya Hijiri <shinya.hijiri@gmail.com>
2017-05-11 19:31:06 +0900

    REST できた

--- src/main/java/hello/RestSampleController.java
+++ src/main/java/hello/RestSampleController.java
@@ -1,7 +1,14 @@
 package hello;
 
-/**
- * Created by hijiri on 2017/05/11.
- */
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
 public class RestSampleController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello!!";
+    }
 }
