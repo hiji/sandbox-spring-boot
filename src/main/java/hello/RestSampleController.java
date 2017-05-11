f1633b69187a0895e39e088a37a296ba1bf72eda
Shinya Hijiri <shinya.hijiri@gmail.com>
2017-05-12 05:51:03 +0900

    XML読むようにした(空になってしまっている)

--- src/main/java/hello/RestSampleController.java
+++ src/main/java/hello/RestSampleController.java
@@ -1,7 +1,12 @@
 package hello;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.io.IOException;
 
 
 @RestController
@@ -15,5 +20,13 @@ public class RestSampleController {
     @GetMapping("/account")
     public Account account() {
         return new Account("hoge", 20);
+    }
+
+    @GetMapping("/xml")
+    public Object xml() throws IOException {
+        XmlMapper xmlMapper = new XmlMapper();
+        xmlMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        Trade trace = xmlMapper.readValue(new File("/Users/hijiri/Downloads/sample.xml"), Trade.class);
+        return trace;
     }
 }
---
+++ src/main/java/hello/Trade.java
@@ -0,0 +1,17 @@
+package hello;
+
+/**
+ * Created by hijiri on 2017/05/11.
+ */
+public class Trade {
+
+    String otherHeader;
+
+    public String getOtherHeader() {
+        return otherHeader;
+    }
+
+    public void setOtherHeader(String otherHeader) {
+        this.otherHeader = otherHeader;
+    }
+}
